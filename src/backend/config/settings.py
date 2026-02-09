2d88127b5a86ae668619b400ff06792f196f8616
Esat <saygina7@gmail.com>
2026-02-09 22:55:21 +0300

    feat(backend): User modeli ekle
    - 'users' isimli django app oluştur
    - User isimli modeli oluştur (AbstractUser)
    - Yeni User modelini Auth User model olarak tanımla

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     # local apps
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
 
 
 REST_FRAMEWORK = {
