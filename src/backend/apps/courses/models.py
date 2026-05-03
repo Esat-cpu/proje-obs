1bb2fd914826d1dd18663f0976a0f86b40389506
Knapeys <arda.yarayan6@gmail.com>
2026-05-03 16:52:45 +0300

    refactor: modelleri güncelle

--- src/backend/apps/courses/models.py
+++ src/backend/apps/courses/models.py
@@ -24,12 +24,10 @@ class Ders(models.Model):
 class DonemDersi(models.Model):
     ders = models.ForeignKey(Ders, on_delete=models.PROTECT)
     akademisyen = models.ForeignKey(Akademisyen, on_delete=models.PROTECT)
-
     yil = models.IntegerField(validators=[MinValueValidator(2000), MaxValueValidator(2100)])
     donem = models.CharField(max_length=10, choices=Donem.choices)
-
     kontenjan = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999)])
-    aktiflik_durumu = models.BooleanField(default=True)
+    aktiflik_durumu = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "Dönem Dersi"
@@ -44,27 +42,5 @@ class DonemDersi(models.Model):
     def __str__(self):
         return f"{self.ders.ad} - {self.donem} {self.yil}"
 
-    def is_active(self):
+    def aktif_mi(self):
         return self.aktiflik_durumu
-
-    def onaylanan_sayisi(self):
-        from apps.enrollments.models import DersKaydi
-
-        return DersKaydi.objects.filter(
-            donem_dersi=self,
-            onay_durumu=True
-        ).count()
-
-    def is_full(self):
-        if self.kontenjan == 0:
-            return False
-        return self.onaylanan_sayisi() >= self.kontenjan
-
-    def kalan_kontenjan(self):
-        kalan = self.kontenjan - self.onaylanan_sayisi()
-        return max(kalan, 0)
-
-    def kontenjan_doluluk_orani(self):
-        if self.kontenjan <= 0:
-            return 0
-        return self.onaylanan_sayisi() / self.kontenjan
