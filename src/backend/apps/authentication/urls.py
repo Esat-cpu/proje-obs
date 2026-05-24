7fa8cf7f843c7b6b1dadc108bc75aeb089642d82
Esat <saygina7@gmail.com>
2026-05-24 12:44:17 +0300

    test(backend): authentication app testleri ekle

--- src/backend/apps/authentication/urls.py
+++ src/backend/apps/authentication/urls.py
@@ -5,7 +5,12 @@ from .views import CustomTokenObtainPairView, LogoutView
 
 
 urlpatterns = [
+    # POST /api/auth/token/
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+
+    # POST /api/auth/token/refresh/
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    # POST /api/auth/logout/
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
